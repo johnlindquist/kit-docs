42300de8ec247c4cdb1dc280a788c54cada150fc
John Lindquist <johnlindquist@gmail.com>
2021-11-15 15:30:51 -0700

    fix(create-docs): uncomment

--- scripts/create-docs.ts
+++ scripts/create-docs.ts
@@ -4,186 +4,186 @@ import { getMetadata } from "@johnlindquist/kit/core/utils"
 let { gql, GraphQLClient } = await npm("graphql-request")
 let slugify = await npm("slugify")
 console.log("👁 made it past npm")
-// interface Author {
-//   login: string
-//   avatarUrl: string
-//   url: string
-//   resourcePath: string
-//   name: string
-//   twitterUsername: string
-// }
+interface Author {
+  login: string
+  avatarUrl: string
+  url: string
+  resourcePath: string
+  name: string
+  twitterUsername: string
+}
 
-// export interface Discussion {
-//   id: string
-//   title: string
-//   author: Author
-//   url: string
-//   body: string
-//   slug: string
-//   createdAt: string
-// }
+export interface Discussion {
+  id: string
+  title: string
+  author: Author
+  url: string
+  body: string
+  slug: string
+  createdAt: string
+}
 
-// export enum Extension {
-//   md = ".md",
-//   js = ".js",
-//   ts = ".ts",
-//   mjs = ".mjs",
-// }
+export enum Extension {
+  md = ".md",
+  js = ".js",
+  ts = ".ts",
+  mjs = ".mjs",
+}
 
-// export enum Category {
-//   Announcements = "MDE4OkRpc2N1c3Npb25DYXRlZ29yeTMyODIwMDgw",
-//   Guide = "MDE4OkRpc2N1c3Npb25DYXRlZ29yeTMyODc5NjIx",
-//   Share = "MDE4OkRpc2N1c3Npb25DYXRlZ29yeTMyMDg0MTcw",
-//   Docs = "DIC_kwDOEu7MBc4B_u-c",
-// }
+export enum Category {
+  Announcements = "MDE4OkRpc2N1c3Npb25DYXRlZ29yeTMyODIwMDgw",
+  Guide = "MDE4OkRpc2N1c3Npb25DYXRlZ29yeTMyODc5NjIx",
+  Share = "MDE4OkRpc2N1c3Npb25DYXRlZ29yeTMyMDg0MTcw",
+  Docs = "DIC_kwDOEu7MBc4B_u-c",
+}
 
-// let endpoint = "https://api.github.com/graphql"
+let endpoint = "https://api.github.com/graphql"
 
-// const categoryKey: keyof Category = await arg<
-//   keyof Category
-// >("Category", Object.keys(Category))
+const categoryKey: keyof Category = await arg<
+  keyof Category
+>("Category", Object.keys(Category))
 
-// console.log({categoryKey})
+console.log({ categoryKey })
 
-// let category = {
-//   name: categoryKey as string,
-//   value: (Category as any)[categoryKey] as string,
-// }
+let category = {
+  name: categoryKey as string,
+  value: (Category as any)[categoryKey] as string,
+}
 
-// let client = new GraphQLClient(endpoint, {
-//   headers: {
-//     "GraphQL-Features": "discussions_api",
-//     authorization: `Bearer ${await env(
-//       "DISCUSSIONS_TOKEN"
-//     )}`,
-//   },
-// })
+let client = new GraphQLClient(endpoint, {
+  headers: {
+    "GraphQL-Features": "discussions_api",
+    authorization: `Bearer ${await env(
+      "DISCUSSIONS_TOKEN"
+    )}`,
+  },
+})
 
-// let query = gql`
-//   query ($categoryId: ID) {
-//     repository(owner: "johnlindquist", name: "kit") {
-//       discussions(
-//         first: 100
-//         categoryId: $categoryId
-//         orderBy: { field: CREATED_AT, direction: DESC }
-//       ) {
-//         # type: DiscussionConnection
-//         totalCount # Int!
-//         nodes {
-//           title
-//           url
-//           author {
-//             ... on User {
-//               twitterUsername
-//               name
-//             }
-//             login
-//             avatarUrl
-//             url
-//           }
-//           body
-//           id
-//           createdAt
-//         }
-//       }
-//     }
-//   }
-// `
+let query = gql`
+  query ($categoryId: ID) {
+    repository(owner: "johnlindquist", name: "kit") {
+      discussions(
+        first: 100
+        categoryId: $categoryId
+        orderBy: { field: CREATED_AT, direction: DESC }
+      ) {
+        # type: DiscussionConnection
+        totalCount # Int!
+        nodes {
+          title
+          url
+          author {
+            ... on User {
+              twitterUsername
+              name
+            }
+            login
+            avatarUrl
+            url
+          }
+          body
+          id
+          createdAt
+        }
+      }
+    }
+  }
+`
 
-// let response = await client.request(query, {
-//   categoryId: category.value,
-// })
+let response = await client.request(query, {
+  categoryId: category.value,
+})
 
-// let discussions: Discussion[] =
-//   response.repository.discussions.nodes.map(
-//     (d: Discussion) => {
-//       const slug = slugify(d.title, {
-//         lower: true,
-//         strict: true,
-//       })
+let discussions: Discussion[] =
+  response.repository.discussions.nodes.map(
+    (d: Discussion) => {
+      const slug = slugify(d.title, {
+        lower: true,
+        strict: true,
+      })
 
-//       return {
-//         ...d,
-//         slug,
-//       }
-//     }
-//   )
+      return {
+        ...d,
+        slug,
+      }
+    }
+  )
 
-// let loadedScripts = discussions.map(
-//   ({
-//     author,
-//     body,
-//     createdAt,
-//     id,
-//     slug,
-//     title,
-//     url: discussion,
-//   }) => {
-//     let url =
-//       body.match(
-//         /(?<=Install.*)https:\/\/gist.*js(?=\"|\))/gim
-//       )?.[0] || ""
-//     let metadata = getMetadata(body)
+let loadedScripts = discussions.map(
+  ({
+    author,
+    body,
+    createdAt,
+    id,
+    slug,
+    title,
+    url: discussion,
+  }) => {
+    let url =
+      body.match(
+        /(?<=Install.*)https:\/\/gist.*js(?=\"|\))/gim
+      )?.[0] || ""
+    let metadata = getMetadata(body)
 
-//     let [, dir, file] = body.match(
-//       /(?<=<meta path=")(.*)\/(.*)(?=")/
-//     ) || [null, "", ""]
+    let [, dir, file] = body.match(
+      /(?<=<meta path=")(.*)\/(.*)(?=")/
+    ) || [null, "", ""]
 
-//     let [description] = body.match(
-//       /(?<=<meta description=")(.*)(?=")/
-//     ) || [""]
-//     let [tag] = body.match(/(?<=<meta tag=")(.*)(?=")/) || [
-//       "",
-//     ]
+    let [description] = body.match(
+      /(?<=<meta description=")(.*)(?=")/
+    ) || [""]
+    let [tag] = body.match(/(?<=<meta tag=")(.*)(?=")/) || [
+      "",
+    ]
 
-//     let content = body
-//     let prevLength = 0
+    let content = body
+    let prevLength = 0
 
-//     let i = 0
-//     for (let s of body.matchAll(
-//       /(`{3}js)(.{5,}?)(`{3})/gs
-//     )) {
-//       i++
-//       if (s[2] && s.index) {
-//         let c = Buffer.from(s[2]).toString("base64url")
-//         let name = `${slug}-example-${i}`
-//         let link = `\n\n[Create script from example below](kit:snippet?name=${name}&content=${c})\n`
+    let i = 0
+    for (let s of body.matchAll(
+      /(`{3}js)(.{5,}?)(`{3})/gs
+    )) {
+      i++
+      if (s[2] && s.index) {
+        let c = Buffer.from(s[2]).toString("base64url")
+        let name = `${slug}-example-${i}`
+        let link = `\n\n[Create script from example below](kit:snippet?name=${name}&content=${c})\n`
 
-//         let index = s.index + prevLength
-//         content = [
-//           content.slice(0, index),
-//           link,
-//           content.slice(index),
-//         ].join("")
-//         prevLength += link.length
-//       }
-//     }
+        let index = s.index + prevLength
+        content = [
+          content.slice(0, index),
+          link,
+          content.slice(index),
+        ].join("")
+        prevLength += link.length
+      }
+    }
 
-//     return {
-//       ...metadata,
-//       avatar: author.avatarUrl,
-//       user: author.login,
-//       author: author.name,
-//       twitter: author.twitterUsername,
-//       discussion,
-//       url,
-//       title,
-//       command: slug,
-//       content,
-//       extension: Extension.md,
-//       dir,
-//       file,
-//       description,
-//       tag,
-//     }
-//   }
-// )
+    return {
+      ...metadata,
+      avatar: author.avatarUrl,
+      user: author.login,
+      author: author.name,
+      twitter: author.twitterUsername,
+      discussion,
+      url,
+      title,
+      command: slug,
+      content,
+      extension: Extension.md,
+      dir,
+      file,
+      description,
+      tag,
+    }
+  }
+)
 
-// await ensureDir(path.resolve("data"))
-// await outputJson(
-//   path.resolve(
-//     "data",
-//     `${category.name.toLowerCase()}.json`
-//   ),
-//   loadedScripts
-// )
+await ensureDir(path.resolve("data"))
+await outputJson(
+  path.resolve(
+    "data",
+    `${category.name.toLowerCase()}.json`
+  ),
+  loadedScripts
+)
